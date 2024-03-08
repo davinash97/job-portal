e979211e070d1f4c2a63b91ed637318e45266548
davinash97 <avinash18.1234@gmail.com>
2024-03-08 21:57:53 +0530

    Update - Responsive UI - Added Google Font and Icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import './App.css'
 
 export default function App() {
+    const submit = () => {
+    window.alert("Submit");
+  }
 
   return (
-    <h1 className='h-screen flex items-center justify-center text-7xl'>Hello World!</h1>
-  )
+      <main className="flex flex-col w-screen h-screen items-center justify-center">
+          <label className="flex flex-col text-center py-10 px-20 rounded-2xl gap-5 m-30">
+              Job Connect
+              <form className="flex justify-center sm:flex-col md:flex-row min-w-full">
+                  <input
+                      type="text"
+                      className="text-center shadow-sm focus:ring-2"
+                      name="search"
+                      id="text"
+                      placeholder="Your Job Search Here"
+                      required={true}
+                  />
+                  <input
+                      type="text"
+                      className="text-center shadow-sm focus:ring-2"
+                      name="location"
+                      id="location"
+                      placeholder="City, State, Remote"
+                      required={true}
+                  />
+                  <button
+                      type="submit"
+                      className="flex items-center justify-center"
+                      onSubmit={submit}>
+                      <span className="material-symbols-outlined">search</span>
+                  </button>
+              </form>
+          </label>
+      </main>
+  );
 }
-
